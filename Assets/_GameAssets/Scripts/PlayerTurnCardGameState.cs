9b8fd629112ae69c749ef475852a44c54eda9e0a
Henry Roberts <whroberts5@gmail.com>
2021-10-31 14:48:54 -0500

    playerTurn

---
+++ Assets/_GameAssets/Scripts/PlayerTurnCardGameState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTurnCardGameState : CardGameState
+{
+    [SerializeField] Text _playerTurnTextUI = null;
+
+    private int _playerTurnCount = 0;
+
+    public override void Enter()
+    {
+        Debug.Log("Player Turn: ...Entering");
+        _playerTurnTextUI.gameObject.SetActive(true);
+
+        _playerTurnCount++;
+        _playerTurnTextUI.text = "Player Turn: " + _playerTurnCount.ToString();
+    }
+
+    public override void Exit()
+    {
+        _playerTurnTextUI.gameObject.SetActive(false);
+        Debug.Log("Player Turn: Exiting...");
+    }
+}
