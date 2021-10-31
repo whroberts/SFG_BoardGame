9ba9b899c4a8778506945535fc78a1bb9162043c
Henry Roberts <whroberts5@gmail.com>
2021-10-31 14:02:07 -0500

    initial state machine

---
+++ Assets/_GameAssets/Scripts/CardGameSM.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGameSM : StateMachine
+{
+    private void Start()
+    {
+        // set starting State here
+    }
+
+}
